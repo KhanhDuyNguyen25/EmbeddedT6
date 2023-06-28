39548dd3a4cd55705677ef171631155f96055db1
KhanhDuyNguyen25 <124339492+KhanhDuyNguyen25@users.noreply.github.com>
2023-06-29 06:10:48 +0700

    update

---
+++ Code C/BaiTapC_28_6/DaoNguocChuoi/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "Header/header.h"
+
+int main(int argc, char const *argv[])
+{  
+    int i = 0;
+    char arr[] = "Nguyen Khanh Duy";
+
+    printf("Kich thuoc chuoi: %d\n", Lengh(arr) );
+
+    printf("INPUT: %s\n", arr);
+
+    ReveresWord(arr);
+
+    printf("OUTPUT: %s", arr);
+    return 0;
+}
---
+++ Code C/BaiTapC_28_6/Header/header.h
@@ -0,0 +1,7 @@
+#include "Source/Source.c"
+
+int Lengh(char *str);
+
+void ReverseString(char *arr, int start, int end);
+
+void ReverseWord(char *arr);
