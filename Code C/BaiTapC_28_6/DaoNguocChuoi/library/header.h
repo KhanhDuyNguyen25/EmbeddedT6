4202ed0a8501a66bb2b8aebe9c851ed2600d651c
KhanhDuyNguyen25 <124339492+KhanhDuyNguyen25@users.noreply.github.com>
2023-06-29 12:27:39 +0700

    Update

--- Code C/BaiTapC_28_6/DaoNguocChuoi/library/header.h
+++
@@ -1,7 +0,0 @@
-#include "source.c"
-
-int Lengh(char *str);
-
-void ReverseString(char *arr, int start, int end);
-
-void ReverseWord(char *arr);
--- Code C/BaiTap_C_28_6/DaoNguocChuoi/main.c
+++ Code C/BaiTap_C_28_6/DaoNguocChuoi/main.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 #include "library/header.h"
+
 int main(int argc, char const *argv[])
 {  
     int i = 0;
     char arr[] = "Nguyen Khanh Duy";
 
-    printf("Kich thuoc chuoi: %d\n", Lengh(arr) );
+    printf("Kich thuoc chuoi: %d\n", LenghString(arr) );
 
     printf("INPUT: %s\n", arr);
 
